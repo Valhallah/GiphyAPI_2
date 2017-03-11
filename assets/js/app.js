e5e907194ea2f8e4c757da6d169bd7bbc10fad0d
ValHall <Valynhall@gmail.com>
2017-03-11 08:42:08 -0500

    pause fix - making gifs appear still and then animate when clicked

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,9 +3,8 @@ $(document).ready(function(){
 
 //variables
 //======================
-var topics = ["Wolverine", "Deadpoole", "Jean"];
-
-
+var topics = ["Wolverine", "Deadpoole", "Jean Grey"];
+//looping through array and adding buttons to the page
 function addButton() {
   $("#buttonDump").empty();
   for (var i = 0; i < topics.length; i++) {
@@ -27,25 +26,25 @@ addButton();
 //main processes
 //======================
 
-//need to render buttons through array - not html
+//when we click the submit putton the topic that was entered will be pushed to the array
 $("#newMutantButton").on("click", function(){
    var userInput = $("#mutantInput").val();
     topics.push(userInput);
-    console.log(topics);
+    //and a button will be added
     addButton();
-
+    //clear out the input feild
     $("#mutantInput").val("");
 });
 
 $("#buttonDump").on("click", "button", function() {
-   
+    //adding the button to the correct div
     addButton();
       // getting the data-mutant attribute
       var xmen = $(this).attr("data-mutant");
       // storing query in the queryURL variablw
       var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
         xmen + "&api_key=dc6zaTOxFJmzC&limit=10";
-       
+
        //ajax call
       $.ajax({
           url: queryURL,
@@ -56,28 +55,43 @@ $("#buttonDump").on("click", "button", function() {
           //we store the data from the ajax response here
           var results = response.data;
 
-          //loop through through the results 
+          //loop through through the results
           for (var i = 0; i < results.length; i++) {
             //here we create and store a div in the mutantDiv variable
             var mutantDiv = $("<div>");
-            //here we create a p tag wich will display the rating 
+
+            //here we create a p tag wich will display the rating
             var p = $("<p>").text("Rating: " + results[i].rating);
             //creating an image tag and storing it in the variable mutantImg
             var mutantImg = $("<img>");
+
             //setting the img attribute
-            mutantImg.attr("src", results[i].images.fixed_height.url);
+            mutantImg.attr("src", results[i].images.fixed_height_still.url);
+            mutantImg.attr("data-still", results[i].images.fixed_height_still.url);
+            mutantImg.attr("data-animate", results[i].images.fixed_height.url);
+            mutantImg.attr("data-state", "still");
+            $(mutantImg).addClass("mutantGif");
 
             //appending p and div to mutantDiv
             mutantDiv.append(p);
             mutantDiv.append(mutantImg);
-            //prepending mutantDiv 
+            //prepending mutantDiv to the correct div
             $("#gifDump").prepend(mutantDiv);
+
+            //when we click the gif
+            $(".mutantGif").on("click", function() {
+              //if the gif is still
+              if ($(this).attr("data-state") == "still") {
+                //animate the gif when clicked
+                $(this).attr("data-state","animate");
+                $(this).attr("src", $(this).attr("data-animate"));
+              } else {
+                //otherwise - when clicked - the image will be still
+                $(this).attr("data-state","still");
+                $(this).attr("src", $(this).attr("data-still"));
+              }
+            });
           }
         });
     });
 });
-
-
-
-
-
